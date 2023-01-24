c2d89e03b0e22a4ddd3452bde07fba2b6fc54498
Evgeniy Shapechko <shapen86@mail.ru>
2023-01-24 18:20:23 +0300

    finished orderController

--- src/modules/controllers/cartController.js
+++ src/modules/controllers/cartController.js
@@ -7,7 +7,7 @@ import { renderNavigation } from "../render/renderNavigation";
 import { renderOrder } from "../render/renderOrder";
 import { renderProducts } from "../render/renderProducts";
 
-const cartGoodsStore = {
+export const cartGoodsStore = {
   goods: [],
   _add(product) {
     if (!this.goods.some((item) => item.id === product.id)) {
@@ -93,6 +93,10 @@ export const removeCart = (product) => {
 
   localStorage.setItem("cart", JSON.stringify(productList));
   return true;
+};
+
+export const clearCart = () => {
+  localStorage.removeItem("cart");
 };
 
 export const cartController = async () => {
